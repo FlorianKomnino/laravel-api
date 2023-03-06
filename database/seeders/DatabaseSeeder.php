203cc4eb7930db8765c28f2604d01618ad4009da
FlorianKomnino <florian.komnino@outlook.it>
2023-03-06 19:05:31 +0100

    added RolesSeeder and UsersSeeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             TypeSeeder::class,
             TechnologiesTableSeeder::class,
+            UsersSeeder::class,
+            RoleSeeder::class,
             ProjectsTableSeeder::class,
             ProjectTechnologySeeder::class,
         ]);
